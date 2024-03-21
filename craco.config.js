3913699b2ac92f3bc721b03166524834d0d85458
yk-a111y <yk19980811@163.com>
2024-03-21 11:03:38 +0800

    feat: 样式重置

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin
+    }
+  ],
   webpack: {
     alias: {
       '@': path.join(__dirname, 'src')
